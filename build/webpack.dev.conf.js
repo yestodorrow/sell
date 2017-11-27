9a81be8c0b809223cd4c7ee7cfdca1e18aaf66cf
yestodorrow <power998@163.com>
2017-11-27 18:16:15 +0800

    文件骨架搭建；出错：cnpm run dev;可能是build下的文件出错

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -21,8 +21,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     historyApiFallback: true,
     hot: true,
     compress: true,
-    host: process.env.HOST || config.dev.host,
-    port: process.env.PORT || config.dev.port,
+    host: process.env.HOST || config.dev.host,
+    port: process.env.PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
     overlay: config.dev.errorOverlay ? {
       warnings: false,
@@ -53,26 +53,26 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
 module.exports = new Promise((resolve, reject) => {
   portfinder.basePort = process.env.PORT || config.dev.port
-  portfinder.getPort((err, port) => {
-    if (err) {
-      reject(err)
-    } else {
-      // publish the new Port, necessary for e2e tests
-      process.env.PORT = port
-      // add port to devServer config
-      devWebpackConfig.devServer.port = port
+portfinder.getPort((err, port) => {
+  if (err) {
+  reject(err)
+} else {
+  // publish the new Port, necessary for e2e tests
+  process.env.PORT = port
+  // add port to devServer config
+  devWebpackConfig.devServer.port = port
 
-      // Add FriendlyErrorsPlugin
-      devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
-        compilationSuccessInfo: {
-          messages: [`Your application is running here: http://${config.dev.host}:${port}`],
-        },
-        onErrors: config.dev.notifyOnErrors
-        ? utils.createNotifierCallback()
-        : undefined
-      }))
+  // Add FriendlyErrorsPlugin
+  devWebpackConfig.plugins.push(new FriendlyErrorsPlugin({
+    compilationSuccessInfo: {
+      messages: [`Your application is running here: http://${config.dev.host}:${port}`],
+    },
+  onErrors: config.dev.notifyOnErrors
+    ? utils.createNotifierCallback()
+    : undefined
+}))
 
-      resolve(devWebpackConfig)
-    }
-  })
+resolve(devWebpackConfig)
+}
+})
 })
